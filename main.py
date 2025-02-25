d60cad81d3553ca0ca3ea8b4a9ae76eae58f2847
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-02-25 18:25:33 +0900

    ✨ test network

---
+++ main.py
@@ -0,0 +1,11 @@
+from torch_multiplication_network import TorchMultiplicationNetwork
+
+def main():
+    epoch = 10
+    network = TorchMultiplicationNetwork()
+    input_data, output_data = network.create_training_data()
+    network.train(input_data, output_data, epoch)
+    network.test()
+
+if __name__ == "__main__":
+    main()
